fdafc96dfbf91a2e10733cd41a79018d19513269
lettucegoblin <77356082+lettucegoblin@users.noreply.github.com>
2024-04-28 14:38:42 -0400

    node-pty, refactoring, fix race condition
    Co-authored-by: Avi Drucker <avidrucker@users.noreply.github.com>

--- lcccloud-front/src/App.jsx
+++ lcccloud-front/src/App.jsx
@@ -15,113 +15,144 @@ const demoBprogram = `    lea r0, hello
 hello:  .string "Hello World!"`;
 
 function App() {
-  const outputRef = useRef(null);
-  const socketRef = useRef(null);
+    const outputRef = useRef(null);
+    const socketRef = useRef(null);
+    const [ideContent, setIdeContent] = useState(localStorage.getItem('ideContent') || defaultCodeString);
+    const [terminalOutput, setTerminalOutput] = useState(localStorage.getItem('terminalOutput') || '');
+    const [fileNameContent, setFileNameContent] = useState(localStorage.getItem('fileNameContent') || 'demoA.a');
+    const [errorMessage, setErrorMessage] = useState("");
+    const [userInput, setUserInput] = useState("");
+    const [runningProgram, setRunningProgram] = useState(false);
 
-  const [textAreaContent, setTextAreaContent] = useState(localStorage.getItem('textAreaContent') || defaultCodeString);
-  const [output, setOutput] = useState(localStorage.getItem('output') || '');
-  const [fileNameContent, setFileNameContent] = useState(localStorage.getItem('fileNameContent') || 'demoA.a');
-  const [errorMessage, setErrorMessage] = useState("");
+    useEffect(() => {
+        outputRef.current.scrollTop = outputRef.current.scrollHeight;
+    }, [terminalOutput]);
 
-  useEffect(() => {
-    outputRef.current.scrollTop = outputRef.current.scrollHeight;
-  }, [output]);
+    useEffect(() => {
+        localStorage.setItem('fileNameContent', fileNameContent);
+        localStorage.setItem('ideContent', ideContent);
+        localStorage.setItem('terminalOutput', terminalOutput);
+    }, [fileNameContent, ideContent, terminalOutput]);
 
-  useEffect(() => {
-    localStorage.setItem('fileNameContent', fileNameContent);
-    localStorage.setItem('textAreaContent', textAreaContent);
-    localStorage.setItem('output', output);
-  }, [fileNameContent, textAreaContent, output]);
+    useEffect(() => {
+        socketRef.current = io(import.meta.env.VITE_LCC_API);
 
-  useEffect(() => {
-    // Connect to the server on component mount and clean up on unmount
-    socketRef.current = io(import.meta.env.VITE_LCC_API);
+        socketRef.current.on('terminal-output', data => {
+            console.log(`LCC Running: ${data.lccRunning}`);
+            setRunningProgram(data.lccRunning);
+            setTerminalOutput(output => `${output}${data.tokenResponse}`);
+        });
 
-    socketRef.current.on('output', data => {
-      setOutput(output => `${output}\n${data.data}`);
-    });
+        socketRef.current.on('terminate-lcc', data => {
+            console.log(`LCC Running: ${data.lccRunning}`);
+            setRunningProgram(false);
+        });
 
-    socketRef.current.on('error', data => {
-      setErrorMessage(`Error: ${data.data}`);
-    });
+        socketRef.current.on('error', data => {
+            setErrorMessage(`Error: ${data.data}`);
+            setRunningProgram(false);
+        });
 
-    socketRef.current.on('completed', () => {
-      console.log('Execution completed.');
-    });
+        return () => {
+            socketRef.current.disconnect();
+        };
+    }, []);
 
-    return () => {
-      socketRef.current.disconnect();
-    };
-  }, []);
-
-  const handleDemoABtnClick = () => {
-    setTextAreaContent(defaultCodeString);
-    setFileNameContent("demoA.a");
-    setErrorMessage('');
-  }
-
-  const handleDemoBBtnClick = () => {
-    setTextAreaContent(demoBprogram);
-    setFileNameContent("demoB.a");
-    setErrorMessage('');
-  }
-
-  const handleClearBtnClick = () => {
-    setOutput('');
-    setErrorMessage('');
-  }
-
-  const handleButtonClick = () => {
-    if (socketRef.current) {
-      socketRef.current.emit('execute', {
-        fileName: fileNameContent,
-        aCode: textAreaContent
-      });
+    const handleDemoABtnClick = () => {
+        setIdeContent(defaultCodeString);
+        setFileNameContent("demoA.a");
+        setErrorMessage('');
     }
-  };
 
-  return (
-    <section className="tc mr-auto ml-auto helvetica">
-      <h1 className="f2 fw9 ma0 pt3 pb2">LCC Cloud</h1>
-      <section>
-        <button className="link pointer dim br-pill bn ph3 pv2 dib mr1" onClick={handleDemoABtnClick}>Load Demo A</button>
-        <button className="link pointer dim br-pill bn ph3 pv2 dib ml1" onClick={handleDemoBBtnClick}>Load Demo B</button>
-      </section>
-      <div className="pt2 measure-narrow flex mr-auto ml-auto">
-        <label className="w4">File Name:</label>
-        <input
-          type="text"
-          placeholder="test.a"
-          className="code w-100"
-          onChange={e => setFileNameContent(e.target.value)}
-          value={fileNameContent} />
-      </div>
-      <section className="measure-narrow mr-auto ml-auto">
-        <p className="ma0 pt2 pb1 o-50">Enter your text program below:</p>
-        <textarea
-          rows="10"
-          className="w-100 code v-align-top"
-          onChange={e => setTextAreaContent(e.target.value)}
-          spellCheck="false"
-          value={textAreaContent}
-        ></textarea>
-      </section>
-      <section className="pt2">
-        <button className="link pointer dim br-pill bn ph3 pv2 dib mr1" onClick={handleButtonClick}>Run</button>
-        <button className="link pointer dim br-pill bn ph3 pv2 dib ml1" onClick={handleClearBtnClick}>Clear</button>
-      </section>
-      <section className="measure-narrow mr-auto ml-auto pt2">
-        <textarea
-          rows="10"
-          className="w-100 code v-align-top"
-          readOnly
-          value={output}
-          ref={outputRef}
-        ></textarea>
-        <p className="ma0 pt2">{errorMessage}</p>
-      </section>
-    </section>
-  );
+    const handleDemoBBtnClick = () => {
+        setIdeContent(demoBprogram);
+        setFileNameContent("demoB.a");
+        setErrorMessage('');
+    }
+
+    const handleClearBtnClick = () => {
+        setTerminalOutput('');
+        setErrorMessage('');
+    }
+
+    const handleRunStopButtonClick = () => {
+        if (!runningProgram) {
+            if (socketRef.current) {
+                socketRef.current.emit('execute', {
+                    fileName: fileNameContent,
+                    codeString: ideContent
+                });
+            }
+        } else {
+            if (socketRef.current) {
+                socketRef.current.emit('terminate-lcc');
+            }
+        }
+
+    };
+
+    const handleUserInput = (e) => {
+        if (e.key === 'Enter') {
+            socketRef.current.emit('input', userInput);
+            setUserInput(''); // Clear input field after sending
+        }
+    };
+
+    return (
+        <section className="tc mr-auto ml-auto helvetica">
+            <h1 className="f2 fw9 ma0 pt3 pb2">LCC Cloud</h1>
+            <section>
+                <button className="link pointer dim br-pill bn ph3 pv2 dib mr1" onClick={handleDemoABtnClick}>Load Demo A</button>
+                <button className="link pointer dim br-pill bn ph3 pv2 dib ml1" onClick={handleDemoBBtnClick}>Load Demo B</button>
+            </section>
+            <div className="pt2 measure-narrow flex mr-auto ml-auto">
+                <label className="w4">File Name:</label>
+                <input
+                    type="text"
+                    placeholder="test.a"
+                    className="code w-100"
+                    onChange={e => setFileNameContent(e.target.value)}
+                    value={fileNameContent} />
+            </div>
+            <section className="measure-narrow mr-auto ml-auto">
+                <p className="ma0 pt2 pb1 o-50">Enter your text program below:</p>
+                <textarea
+                    rows="10"
+                    className="w-100 code v-align-top"
+                    onChange={e => setIdeContent(e.target.value)}
+                    spellCheck="false"
+                    value={ideContent}
+                ></textarea>
+                <section className="pt2">
+                    <button
+                        className="link pointer dim br-pill bn ph3 pv2 dib mr1"
+                        onClick={handleRunStopButtonClick}>
+                        {runningProgram ? "Stop" : "Run"}
+                    </button>
+                    <button className="link pointer dim br-pill bn ph3 pv2 dib ml1" onClick={handleClearBtnClick}>Clear</button>
+                </section>
+                <section className="measure-narrow mr-auto ml-auto pt2">
+                    <textarea
+                        id="lcc-cloud-output"
+                        rows="10"
+                        className="w-100 code v-align-top"
+                        readOnly
+                        value={terminalOutput}
+                        ref={outputRef}
+                    ></textarea>
+                    <input
+                        type="text"
+                        placeholder="Enter input..."
+                        className="w-100 mt3"
+                        value={userInput}
+                        onChange={e => setUserInput(e.target.value)}
+                        onKeyPress={handleUserInput}
+                    />
+                    <p className="ma0 pt2">{errorMessage}</p>
+                </section>
+            </section>
+        </section>
+    );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const os = require('os');
 const http = require('http');
 const socketIo = require('socket.io');
+const pty = require('node-pty');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,10 +16,13 @@ const io = new socketIo.Server(server, {
 });
 
 const port = process.env.PORT || 3000;
+const IS_WIN = os.platform() === 'win32';
+const SHELL = IS_WIN ? 'powershell.exe' : 'bash';
+const UNIQUE_MARKER = "CMD_DONE_MARKER";
 
-app.use(cors({origin: '*'}));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(cors({ origin: '*' }))
+    .use(express.json())
+    .use(express.static(path.join(__dirname, 'dist')));
 
 // Function to change permissions
 function changePermissions(filePath, mode) {
@@ -36,15 +40,15 @@ function changePermissions(filePath, mode) {
 }
 
 let lccCommand = './lcc';
-if (os.platform() === 'win32') {
+if (IS_WIN) {
     lccCommand = './lcc.exe';
 } else {
     changePermissions(lccCommand, '755');
 }
 
-function writeAssemblyCodeToFile(fileName, aCode, callback) {
+function writeCodeToFile(fileName, codeString, callback) {
     const fs = require('fs');
-    fs.writeFile(fileName, aCode, (error) => {
+    fs.writeFile(fileName, codeString, (error) => {
         if (error) {
             console.error(error);
             return;
@@ -55,40 +59,113 @@ function writeAssemblyCodeToFile(fileName, aCode, callback) {
 }
 
 function executeLCC(socket, fileName) {
-    const lccProcess = spawn(lccCommand, [fileName]);
-    
-    lccProcess.stdout.on('data', (data) => {
-        socket.emit('output', { data: data.toString() });
-    });
-
-    lccProcess.stderr.on('data', (data) => {
-        socket.emit('error', { data: data.toString() });
-    });
-
-    lccProcess.on('close', (code) => {
-        socket.emit('completed', { code });
-    });
-
-    // Handling input from the client
-    socket.on('input', (input) => {
-        lccProcess.stdin.write(input);
-        lccProcess.stdin.end();
-    });
+    // Execute your command within the terminal
+    console.log('Executing command', socket.ptyTerminal.lccRunning, socket.ptyTerminal.killed);
+    if(!socket.ptyTerminal.lccRunning){
+        socket.ptyTerminal.lccRunning = true; 
+        socket.ptyTerminal.write(`${lccCommand} ${fileName} ${IS_WIN ? ';' : '&&'} echo ${UNIQUE_MARKER}\r`);
+           
+    }
 }
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+function bindPtyTerminalWithSocket(socket){
     
+
+    socket.ptyTerminal = pty.spawn(SHELL, [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 30,
+        cwd: process.cwd(),
+        env: process.env
+    });
+    socket.ptyTerminal.lccRunning = false;
+    socket.ptyTerminal.cleanup = function cleanup(){
+        console.log('Cleaning up');
+        if (socket.ptyTerminal.lccRunning) {
+            socket.ptyTerminal.lccRunning = false;
+            // send ctrl+c to kill the process
+            socket.ptyTerminal.write('\x03');
+            socket.ptyTerminal.write('\x03');
+            socket.uniqueMarkerCount = 0
+            socket.lineCount = 0;
+            socket.processingData = false;
+        }
+    }
+    socket.ptyTerminal.on('error', function (error) {
+        console.error(`Error: ${error}`);
+    });
+    socket.uniqueMarkerCount = 0;
+    socket.lineCount = 0;
+    socket.processingData = false;
+
+    /* lccProcess bindings */
+    socket.ptyTerminal.on('data', function (data) {
+        if(socket.processingData)
+            return; // Skip the data processing if the previous data is still being processed
+        
+        socket.processingData = true;
+
+        if(!socket.ptyTerminal.lccRunning || socket.ptyTerminal.killed) {
+            socket.processingData = false;
+            return;
+        }
+        socket.lineCount++;
+        console.log('Line count:', socket.lineCount, socket.uniqueMarkerCount);
+
+        console.log('\x1b[36m%s\x1b[0m', data); // Print the output in cyan color
+        if (data.includes(UNIQUE_MARKER)) { // Check if the marker is present in the output
+            let markerIndex = data.indexOf(UNIQUE_MARKER);
+            let escapeSequence = "[1C";
+            data = data.replace(UNIQUE_MARKER, ''); // Remove the marker from the output
+            data = data.replace(escapeSequence, " ");
+            if (socket.uniqueMarkerCount > 0) { // If the marker is present more than once, it means the output is complete
+                // When you detect the marker, emit a 'finished' event
+                console.log('Contains unique marker');
+                socket.ptyTerminal.cleanup();
+                socket.emit('terminal-output', { tokenResponse: data.slice(0, markerIndex), lccRunning: socket.ptyTerminal.lccRunning });
+                
+                return;
+            }
+            socket.uniqueMarkerCount++;
+        }
+        if(socket.lineCount <= 1) {
+            console.log('Skipping the first line');
+            socket.processingData = false;
+            return;
+        }
+        socket.emit('terminal-output', { tokenResponse: data, lccRunning: socket.ptyTerminal.lccRunning });
+        socket.processingData = false;
+    });
+
+    socket.ptyTerminal.on('exit', function (code, signal) {
+        console.log(`lccProcess exited with code ${code} and signal ${signal}`);
+    });
+    /* socket bindings */
+    socket.on('input', (input) => {
+        console.log(`Received input: ${input}`)
+        if(!socket.ptyTerminal.lccRunning || socket.ptyTerminal.killed)
+            return;
+        socket.ptyTerminal.write(input + '\r'); // '\r' simulates the enter key in a terminal
+    });
+
+    socket.on('terminate-lcc', () => {
+        socket.ptyTerminal.cleanup(); // sends ctrl-c to kill the process. keeps the terminal clean
+        socket.emit('terminate-lcc', { lccRunning: socket.ptyTerminal.lccRunning });
+    });
+
     socket.on('execute', (data) => {
-        const { fileName, aCode } = data;
-        writeAssemblyCodeToFile(fileName, aCode, () => {
+        const { fileName, codeString } = data;
+        writeCodeToFile(fileName, codeString, () => {
             executeLCC(socket, fileName);
         });
     });
+}
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.uniqueId = Math.random().toString(36).substring(7); // TODO: replace with words/githubid
+    bindPtyTerminalWithSocket(socket);
 });
 
 server.listen(port, () => {
